e9e2108265f169bcc15fd62e828526a0710ad7df
yadav-angad <yadavangad100@gmail.com>
2025-08-28 09:34:52 -0500

    fix gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    filename: 'main.js',
+    publicPath: 'auto',
   },
   mode: 'development',
   devtool: 'inline-source-map',
